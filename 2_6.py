b8859a98993fa927a4549590994a58469fecac71
hwajung <52371573+xxiswajung@users.noreply.github.com>
2023-02-27 16:30:03 +0900

    Update 2_6.py

--- 2_6.py
+++ 2_6.py
@@ -1,7 +1,6 @@
 n=int(input())
 
 def digit_sum(x):
-    # length=len(x)
     sum=0
     while x > 0:
         sum = sum + x%10
@@ -11,10 +10,10 @@ def digit_sum(x):
 maxlist=[]
 arr=list(map(int,input().split()))
 
+/* 내가 푼 방식 ->최대값을 뽑겠다고 불필요한 리스트를 또 생성할 필요는 없다 */
 
 for x in arr :
     maxlist.append(digit_sum(x))
-# maxlist.append(digit_sum(arr[i]))
 
 maxi=max(maxlist)
 
@@ -22,3 +21,12 @@ for index, res in enumerate(maxlist):
     if maxi == res :
         print(arr[index])
         break
+        
+/* 풀이 */
+
+for x in arr:
+    res = digit_sum(x)
+    if max < res :
+        max = res
+        p = x
+print(p)
