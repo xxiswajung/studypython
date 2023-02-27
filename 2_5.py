c18b504ff1bc1d14631369eaff49d831aa8d8409
hwajung <52371573+xxiswajung@users.noreply.github.com>
2023-02-27 16:04:29 +0900

    Update 2_5.py

--- 2_5.py
+++ 2_5.py
@@ -6,7 +6,8 @@ for i in range(1,n+1):
     for j in range(1,m+1):
         cnt[i+j]=cnt[i+j]+1 #합의 결과가 나올때마다 가중치 +1
 
-res = max(list)
+res = max(cnt)
+
 for i in range(n+m+1):
     if cnt[i]==max: #가장 많이 나온 것들을 뽑아서
         print(i, end=' ') # !!!하나씩!!! 출력, 굳이 list로 축적하지 않아도 됨
