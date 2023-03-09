f7d46da2cd835b8a8c66df49b288c08c44b7231b
hwajung <52371573+xxiswajung@users.noreply.github.com>
2023-03-09 18:23:44 +0900

    Update 3_10.py
    Add more compact code

--- ch3/3_10.py
+++ ch3/3_10.py
@@ -35,3 +35,31 @@ if flag == 0:
     print("YES")
 else :
     print("NO")
+
+    
+#More compact
+# def check(a):
+#     for i in range(9): #행과 열 체크
+#         ch1=[0]*10
+#         ch2=[0]*10
+#         for j in range(9):
+#             ch1[a[i][j]]=1
+#             ch2[a[j][i]]=1
+#         if sum(ch1)!=9 or sum(ch2)!=9:
+#             return False
+#     for i in range(3): #3x3 정사각형 체크
+#         for j in range(3):
+#             ch3=[0]*10
+#             for k in range(3):
+#                 for s in range(3):
+#                     ch3[a[i*3+k][j*3+s]]=1
+#             if sum(ch3)!=9:
+#                 return False
+#     return True
+
+# a = [list(map(int,input().split())) for _ in range(9)]
+
+# if check(a):
+#     print("YES")
+# else:
+#     print("NO")
