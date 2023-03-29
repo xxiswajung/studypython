2705aa367e06f45031e25622bed8b956cdf8b998
hwajung <52371573+xxiswajung@users.noreply.github.com>
2023-03-30 01:18:06 +0800

    Update 5_9.py
    add another version

--- ch5/5_9.py
+++ ch5/5_9.py
@@ -14,3 +14,21 @@ if all(value == 0 for value in p.values()):
     print("YES")
 else:
     print("NO")
+    
+#딕셔너리의 내장함수 get 이용하기
+#초기의 value값은 초기화 되어있지 않아 누적이 안되기 때문
+
+a=input()
+b=input()
+str1=dict()
+for x in a:
+    str1[x]=str1.get(x,0)+1
+for x in b:
+    str1[x]=str1.get(x,0)-1
+
+for x in a:
+    if str1.get(x)>0:
+        print("NO")
+        break
+else:
+    print("YES")
