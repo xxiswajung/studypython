800ff133c34ad8e17ae800e291d60773ea0caa3a
hwajung <52371573+xxiswajung@users.noreply.github.com>
2023-04-12 14:40:03 +0900

    Create 6_12.py
    6_9.py 를 라이브러리 활용하여 풀기

---
+++ ch6/6_12.py
@@ -0,0 +1,16 @@
+import itertools as it
+
+n,f=map(int,input().split())
+b=[1]*n
+for i in range(1,n):
+    b[i]=b[i-1]*(n-i)//i
+a=list(range(1,n+1))
+
+for tmp in it.permutations(a):
+    sum=0
+    for L,x in enumerate(tmp):
+        sum+=(b[L]*x)
+    if sum==f:
+        for x in tmp:
+            print(x, end=' ')
+        break
