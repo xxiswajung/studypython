3738f10c8665dd29c9813bd459308e9c109d19ed
hwajung <52371573+xxiswajung@users.noreply.github.com>
2023-04-13 17:05:14 +0900

    Update 6_11.py

--- ch6/6_11.py
+++ ch6/6_11.py
@@ -15,3 +15,17 @@ if __name__=="__main__":
     cnt=0
     DFS(0,0,0)
     print(cnt) 
+    
+#combination 라이브러리 사용
+
+import itertools as it
+
+n,k=map(int,input().split())
+a=list(map(int,input().split()))
+m=int(input())
+cnt=0
+
+for x in it.combinations(a,k):
+    if sum(x)%m==0:
+        cnt+=1
+print(cnt)
