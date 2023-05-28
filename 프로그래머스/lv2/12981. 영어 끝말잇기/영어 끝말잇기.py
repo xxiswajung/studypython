9589d50d0d64d3984f62bf31322cfa2687ea65c7
hwajung <52371573+xxiswajung@users.noreply.github.com>
2023-05-28 16:20:09 +0900

    Update 영어 끝말잇기.py

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -12,3 +12,16 @@ def solution(n, words):
     if len(res)==0:
         answer=[0,0]
     return answer
+
+######## more compacted version ########
+# Rather than appending the index of same words to the list, 
+# adding conditional statements to check if the same word has ever appeared before the word appears.
+
+def solution(n, words):
+    answer = []
+    res = []
+    for i in range(1,len(words)):
+        if words[i] in words[:i] or words[i][0]!=words[i-1][-1]:	
+            return [i%n+1, i//n+1]
+    else:
+        return [0,0]
