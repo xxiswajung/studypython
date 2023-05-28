df8e2fc12b308965649aaa6c28f57993f37ac18c
hwajung <52371573+xxiswajung@users.noreply.github.com>
2023-05-28 16:06:09 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.11 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -0,0 +1,14 @@
+def solution(n, words):
+    answer = []
+    res = []
+    for i in range(1,len(words)):
+        if words.count(words[i])==2 or words[i][0]!=words[i-1][-1]:	
+            for j in range(len(words)):
+                if words[j]==words[i]:
+                    res.append(j)
+            answer.append(max(res)%n+1)
+            answer.append(max(res)//n+1)
+            break
+    if len(res)==0:
+        answer=[0,0]
+    return answer
