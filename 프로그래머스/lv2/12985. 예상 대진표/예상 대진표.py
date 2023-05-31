d6163954c89eb23b5a9bd317eff6cd3698a82f80
hwajung <52371573+xxiswajung@users.noreply.github.com>
2023-05-31 20:26:21 +0900

    Update 예상 대진표.py

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.py
@@ -24,4 +24,13 @@ def solution(n,a,b):
                 else:
                     b=b//2
                     answer+=1
-            
+                    
+################################
+#more compact solving
+
+def solution(n,a,b):
+    answer = 0
+    while a!=b:
+        answer+=1
+        a,b=(a+1)//2,b=(b+1)//2
+    return answer
