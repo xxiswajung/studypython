ec17befe0a03ae7cae897513c03c5df8a15a42f0
hwajung <52371573+xxiswajung@users.noreply.github.com>
2023-06-16 21:07:51 +0900

    Update 의상.py

--- 프로그래머스/lv2/42578. 의상/의상.py
+++ 프로그래머스/lv2/42578. 의상/의상.py
@@ -1,6 +1,5 @@
 def solution(clothes):
     answer = 1
-    res = 0
     dict={}
     for i in clothes:
         dict[i[1]]=0
@@ -8,8 +7,4 @@ def solution(clothes):
         dict[i[1]]+=1
     for x in dict:
         answer*=(dict[x]+1)
-        res+=dict[x]
-    if len(dict)==1:
-        return res
-    else:
-        return answer-1
+    return answer-1
