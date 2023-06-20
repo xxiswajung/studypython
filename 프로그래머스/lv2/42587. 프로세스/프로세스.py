0ad3d91a0c114fa46ee50a3cfdcc7eeb3fa0c660
hwajung <52371573+xxiswajung@users.noreply.github.com>
2023-06-20 14:49:47 +0900

    Update 프로세스.py

--- 프로그래머스/lv2/42587. 프로세스/프로세스.py
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.py
@@ -2,7 +2,7 @@ def solution(priorities, location):
     answer = 0
     while priorities:
         if location==0:
-            for i in range(1,len(priorities)):
+            for i in range(1,len(priorities)): 
                 if priorities[0]<priorities[i]:
                     priorities.append(priorities[0])
                     priorities.pop(0)
@@ -21,5 +21,19 @@ def solution(priorities, location):
                 priorities.pop(0)
                 answer+=1
             location-=1
+
+######################################
+
+def solution(priorities, location):
+    answer = 0
+    queue = [(i,p) for i,p in enumerate(priorities)] #Use 'enumearte' instead of moving location manually(=location-=1, location=len(priorities)-1)
+    while True:
+        cur=queue.pop(0)
+        if any(cur[1]<q[1] for q in queue): #Use 'any' instead of 'for' loop.
+            queue.append(cur)
+        else:
+            answer+=1
+            if cur[0]==location:
+                return answer
             
                     
