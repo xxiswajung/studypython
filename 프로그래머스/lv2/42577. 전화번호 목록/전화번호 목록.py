dc4c81c6f8f8ed15306823102e74585c806d802b
hwajung <52371573+xxiswajung@users.noreply.github.com>
2023-06-24 16:11:42 +0900

    Update 전화번호 목록.py

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -4,6 +4,16 @@ def solution(phone_book):
         arr = "" 
         for num in nums: 
             arr += num
-            if arr in dict and arr != nums:       
+            if arr in dict and arr != nums: #누적된 arr와 dict안의 숫자와 같은지(앞에서 부터 더했으니) 확인, 그리고 자기자신인건 확인하지 않는다.
                 return False 
     return True
+
+############################
+def solution(phone_book):
+    answer = True
+    phone_book.sort() #문자열을 정렬할땐 숫자 크기대로 정렬되지 않고, 숫자의 유니코드 순대로 정렬된다
+    for i in range(len(phone_book)-1): 
+        if phone_book[i] == phone_book[i+1][:len(phone_book[i])]: #그러므로 옆에 있는 문자열과만 비교하면 된다
+            answer = False
+            break
+    return answer
