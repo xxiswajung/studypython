8fb2e3b3789327e29ceaa1b27326b1f98ffa29e7
hwajung <52371573+xxiswajung@users.noreply.github.com>
2023-06-28 20:42:19 +0900

    Update 더 맵게.py

--- 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
@@ -14,5 +14,23 @@ def solution(scoville, K):
         answer += 1
         if len(scoville) == 1 and scoville[0] < K:
             return -1
-        
+
+
+#################################
+#런타임 에러 고친 내 방식
+#스코빌 철자 확인..
+
+import heapq
+
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)
+    
+    while True:
+        if len(scoville)==1 and scoville[0]<K:
+            return -1
+        if scoville[0]>=K:
+            return answer
+        heapq.heappush(scoville,heapq.heappop(scoville)+heapq.heappop(scoville)*2)
+        answer+=1
       
