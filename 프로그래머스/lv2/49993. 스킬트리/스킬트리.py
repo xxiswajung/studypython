df624bde469af5dfb6ae78254406acd3f2ffdd0e
hwajung <52371573+xxiswajung@users.noreply.github.com>
2023-07-11 15:55:09 +0900

    Update 스킬트리.py

--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.py
@@ -2,7 +2,10 @@ def solution(skill, skill_trees):
     answer = 0
     
     for skills in skill_trees:
-        skill_list=list(skill)
+        skill_list=list(skill) 
+        #이름을 다르게 해서 리스트를 새로 만드는 이유
+        #만약 이름을 안 바꾸고 그대로 하면, 아래의 pop() 실행이 될때 원래 매개변수로 주어졌던 skill이 비어버려 더이상 문자 비교를 할 수 없기 때문에
+        #주어진 매개변수 skill은 유지하고, 새로 똑같은 내용의 리스트를 만드는 것.
         
         for y in skills:
             if y in skill_list:
