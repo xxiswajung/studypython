ff2a79074ee082ab8a757dce2b7b863c199d5880
hwajung <52371573+xxiswajung@users.noreply.github.com>
2023-07-12 17:56:45 +0900

    Update 모음 사전.py

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.py
@@ -1,3 +1,18 @@
+#단어의 총 개수가 적어 전부 다 구해도 DFS보다 실행시간이 적음
+
+from itertools import product #중복 순열
+
+def solution(word):
+    words=[]
+    for i in range(1,6):
+        for c in product(['A','E','I','O','U'], repeat=i):
+            words.append(''.join(c))
+    words.sort()
+    return words.index(word)+1
+
+##########################################
+#처음엔 DFS로 풀려고 했으나, 실행시간이 더 길고 정답을 발견한 시점에서 바로 answer를 return 하지 못함
+
 answer = 0
 count = 0
 arr = ['A','E','I','O','U']
